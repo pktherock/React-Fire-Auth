557332f1e7b6ae7c246a5bb53de4937778c8deee
pktherock <pktherock00@gmail.com>
2023-11-02 14:58:09 +0530

    fix typos

--- src/app/features/auth/components/Update-Password.jsx
+++ src/app/features/auth/components/Update-Password.jsx
@@ -166,7 +166,7 @@ function UpdatePassword() {
                   type="submit"
                   className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
                 >
-                  Delete me from Database
+                  Update Password
                   <ArrowRightIcon className="ml-2 h-6 w-6" />
                 </button>
               </div>
